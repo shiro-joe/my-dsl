4fd71ebfa8acbf77482fa1640c18efe625447d7e
shirorina <ee227100@meiji.ac.jp>
2025-10-20 17:17:55 +0900

    add dsl.json, dsl.ts and translater.ts

---
+++ src/dsl/dsl.ts
@@ -0,0 +1,26 @@
+"use strict";
+/* テストスイート */
+class Suite {
+  private cases: Case[];
+  private fixtures: Fixture[];
+  constructor(cases: Case[], fixtures: Fixture[]) {
+    this.cases = cases;
+    this.fixtures = fixtures;
+  }
+}
+
+/* テストケース */
+class Case {}
+
+class Fixture {}
+
+/* テストケース内のAAA */
+
+class Assertion {}
+
+class Arrange {}
+
+class Act {}
+
+/* 生 */
+class Other {}
